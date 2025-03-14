837f5812774e9318c359c589c418cca17995e52b
GilbertAU <aryautamagilbert@gmail.com>
2025-03-14 14:55:07 +0700

    .

--- Tugas/Assets/Scripts/PlayerJump.cs
+++ Tugas/Assets/Scripts/PlayerJump.cs
@@ -8,12 +8,12 @@ public class PlayerJump : MonoBehaviour
     public float jumpForce;
     public bool isGrounded;
     public int totalJump;
-    private Rigidbody2b rb;
+    private Rigidbody2D rb;
     [HideInInspector]public int currentTotalJump = 0;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
