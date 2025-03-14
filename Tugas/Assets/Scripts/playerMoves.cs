7d8581ae68f97b337ead7f025baaf80c8ea5432e
GilbertAU <aryautamagilbert@gmail.com>
2025-03-14 14:41:39 +0700

    .

---
+++ Tugas/Assets/Scripts/playerMoves.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMoves : Player
+{
+    public float moveSpeed;
+    private Camera cam;
+    private Rigidbody rb;
+    [HideInInspector] public Vector3 moveDirection;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main.GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(Horizontal, 0f, Vertical).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveDirection * moveSpeed, ForceMode.Impulse);
+    }
+}
